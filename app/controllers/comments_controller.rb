a9fa9ae1e9924184c1751826887ea14082d55d5c
Sophie Mann <sklm@live.co.uk>
2017-05-29 14:37:51 +0100

    updates comments controller specs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
       @post = Post.find(params[:post_id])
       @comment = @post.comments.create(comment_params)
-      redirect_to post_path(@post)
+      redirect_to posts_path
     end
 
     private
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  describe "GET /new " do
-    it "responds with 200" do
-      get :new
-      expect(response).to have_http_status(200)
+  describe "POST /" do
+    before(:each) do
+      @post = Post.create(message: "Hello, world!")
+      @comment = @post.comments.create(commenter: "Bob", body: "Comment")
+    end
+    it "creates a comment" do
+      expect(@post.comments[0].body).to eq("Comment")
     end
   end
 end
