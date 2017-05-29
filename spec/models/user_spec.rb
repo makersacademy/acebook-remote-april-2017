5e5e5df4b7ec88ef3e475a83abfaa8c0d7529abe
Sam <sblausten@Sams-Mac.local>
2017-05-29 13:14:27 +0100

    Added two validity test on User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before(:each){
+		@user = User.new(name: "Sam", email: "test@test.com")
+	}
+
+  describe "setup" do
+  	it "creates a valid user" do
+  		expect(@user.valid?).to be(true)
+  	end
+  	it "checks user has a name attribute" do
+  		expect(@user.name).to eq("Sam")
+  	end
+  end
 end
