11fa4265b989480e247f22147f41cb9394d64e0a
Paula <pmuldoon@umich.edu>
2017-05-29 14:44:08 +0100

    generated Welcome controller & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts
+  get 'welcome/index'
 
-  get '/index', to: 'posts#index'
+  root 'welcome#index'
+
+  resources :posts
 
 end
