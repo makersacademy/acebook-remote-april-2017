d3248ae6dc16f12c85463bb6da54fdb373e7bfc2
Adam Erdem Er <adamerdemer@gmail.com>
2017-05-29 15:06:35 +0100

    Added basic User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	before(:each){
-		@user = User.new(name: "Sam", email: "test@test.com")
-	}
-
+	let(:user){ described_class.new(name: "Sam", email: "test@test.com") }
+	
   describe "setup" do
   	it "creates a valid user" do
-  		expect(@user.valid?).to be(true)
+  		expect(user.valid?).to be(true)
   	end
   	it "checks user has a name attribute" do
-  		expect(@user.name).to eq("Sam")
+  		expect(user.name).to eq("Sam")
   	end
+		it "checks user has a email attribute" do
+			expect(user.email).to eq("test@test.com")
+		end
   end
 end
