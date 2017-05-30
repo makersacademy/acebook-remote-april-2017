e3accdfe9527d2138d9838aa540fa2e52e74ad59
Cristina Iannone <iannonecristina@gmail.com>
2017-05-30 13:02:06 +0100

    Adds private methods to format_url.rb

--- app/models/classes/format_url.rb
+++ app/models/classes/format_url.rb
@@ -1,18 +1,30 @@
 class Classes::FormatUrl
 
   def return_formatted_url(post)
-    if post.include?("http") || post.include?("www")
+    if has_link?(post)
       post_array = post.split(" ")
       post_array.each do |word|
-        if word.start_with?("www")
-          word.replace("<a href=\"http://#{word}\"> #{word} </a>")
-        elsif word.start_with?("http")
-          word.replace("<a href=\"#{word}\"> #{word} </a>")
-        end
+        format_link(word)
       end
       post = post_array.join(" ")
     end
     return post
+  end
+
+  private
+
+  def has_link?(post)
+    if post.include?("http") || post.include?("www")
+      return true
+    end
+  end
+
+  def format_link(word)
+    if word.start_with?("www")
+      word.replace("<a href=\"http://#{word}\"> #{word} </a>")
+    elsif word.start_with?("http")
+      word.replace("<a href=\"#{word}\"> #{word} </a>")
+    end
   end
 
 end
