6d659d5a085325a21294aa75ae7cedbe6ed24ca1
Ana Maria Suciu <anamariasuciu@gmail.com>
2017-05-30 17:00:21 +0200

    Update posts_controller_spec.rb

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
-      #post = create(:post)
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
@@ -25,7 +24,6 @@ RSpec.describe PostsController, type: :controller do
 
     it "creates a post" do
       post = create(:post)
-      #post :create, params: { post: { message: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
 
