ea718f5eba79b9b1acac95d83570bc25ca2f7a89
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-05-30 12:56:35 +0100

    reorganises PostPresenter and refactor

--- app/models/post_presenter.rb
+++
@@ -1,6 +0,0 @@
-module PostPresenter
-
-  def format_time
-    return "posted at #{self.created_at.strftime('%H:%M')} on #{self.created_at.strftime('%d/%m/%Y')}"
-  end
-end
---
+++ app/presenters/post_presenter.rb
@@ -0,0 +1,6 @@
+module PostPresenter
+
+  def format_time
+    "posted at #{self.created_at.strftime('%H:%M')} on #{self.created_at.strftime('%d/%m/%Y')}"
+  end
+end
