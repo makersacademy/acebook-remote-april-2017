1b521180d693a7e35aca4c9257e23cd448507704
Alex Chalk <alexchalk17@gmail.com>
2017-05-30 08:04:47 -0400

    Add feature test for creating single-photo album

---
+++ spec/features/users_can_upload_and_view_photos_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "photo-album", type: :feature do
+  scenario 'user uploads an image' do
+    visit '/photo-albums/new'
+    fill_in 'title', with: "Riot in Brighton"
+    page.attach_file("post_image", Rails.root + 'app/assets/images/brighton.jpeg')
+    click_on 'Submit'
+    expect(page).to have_content 'brighton.jpeg'
+  end
+end
