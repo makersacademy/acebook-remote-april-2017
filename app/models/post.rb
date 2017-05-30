af5cc77ae29199d0be8872718da6a025c0669336
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-05-30 12:06:40 +0100

    adds PostPresenter module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,3 @@
 class Post < ApplicationRecord
-  def format_time
-    return "posted at #{self.created_at.strftime('%H:%M')} on #{self.created_at.strftime('%d/%m/%Y')}"
-  end
+  include PostPresenter
 end
