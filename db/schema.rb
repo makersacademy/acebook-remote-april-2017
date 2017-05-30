01c3c5e70f93c045a2618e054e1d73b2938a2045
Sophie Mann <sklm@live.co.uk>
2017-05-30 16:52:02 +0100

    updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529102847) do
+ActiveRecord::Schema.define(version: 20170529142941) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "post_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
@@ -30,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170529102847) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
