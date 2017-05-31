a10f5134f71b82d67bb69c6ef0125fc377ebfdf9
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-31 17:02:46 +0100

    Adds tests to controller spec

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe GroupsController, type: :controller do
     end
   end
 
+  describe "POST /" do
+    it "responds with 200" do
+      group :create, params: { group: { name: "Group 1" } }
+      expect(response).to redirect_to(groups_url)
+    end
+
+    it "creates a group" do
+      group = create(:group)
+      expect(Group.find_by(name: "Group 1")).to be
+    end
+  end
+
 end
