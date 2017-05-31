2a60401412297bc3871aba321f8f5e6426fc0059
anaalta <suciuanamaria@yahoo.com>
2017-05-31 12:26:39 +0200

    Add image upload functionality

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :image
+  has_attached_file :image, styles: { medium: '400x400>', thumb: '100x100>'}
   validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/png']
+
 end
--- spec/features/users_can_upload_and_view_photos_spec.rb
+++ spec/features/users_can_upload_and_view_photos_spec.rb
@@ -6,6 +6,6 @@ RSpec.feature "photos", type: :feature do
     fill_in 'photo_title', with: 'Riot in Brighton'
     page.attach_file('photo_image', Rails.root + 'app/assets/images/brighton.jpg')
     click_on 'Submit'
-    expect(page).to have_content 'brighton.jpg'
+    expect(page).to have_content 'Riot in Brighton'
   end
 end
