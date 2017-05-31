d83488339967c72482cc07c885d72215665aa68b
milesillsley <milesillsley@hotmail.co.uk>
2017-05-31 17:29:38 +0100

    db: added date column to photo_albums table

--- db/migrate/20170530134528_create_photo_albums.rb
+++ db/migrate/20170530134528_create_photo_albums.rb
@@ -2,7 +2,7 @@ class CreatePhotoAlbums < ActiveRecord::Migration[5.1]
   def change
     create_table :photo_albums do |t|
       t.string :title
-      t.string :location
+
 
       t.timestamps
     end
