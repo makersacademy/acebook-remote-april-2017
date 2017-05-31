2f585a947af9118ac2936c6e9050b795e12bf348
danielemanuel <daniel.costea@vkernel.ro>
2017-05-31 12:09:18 +0100

    All tests green

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -13,11 +13,29 @@ RSpec.feature "Sessions",  type: :feature do
   end
 
   scenario "signs in the user", :type => :feature do
+    visit "/users/sign_up"
+    email = "example@example.com"
+  	fill_in 'user_email', :with => email
+  	fill_in 'user_password', :with => "password"
+  	fill_in 'user_password_confirmation', :with => "password"
+    click_button 'Sign up'
+    visit "/users/sign_out"
     visit "/users/sign_in"
     email = "example@example.com"
     fill_in 'user_email', :with => email
     fill_in 'user_password', :with => "password"
     click_button 'Log in'
     page.should have_content("Signed in successfully.")
+  end
+
+  scenario "signs in the user", :type => :feature do
+    visit "/users/sign_up"
+    email = "example@example.com"
+  	fill_in 'user_email', :with => email
+  	fill_in 'user_password', :with => "password"
+  	fill_in 'user_password_confirmation', :with => "password"
+    click_button 'Sign up'
+    visit "/users/sign_out"
+    page.should have_content("Signed out successfully.")
   end
 end
