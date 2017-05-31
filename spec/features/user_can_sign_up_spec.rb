7075afb8acf51b02e20ccb87e49af77369b1b535
danielemanuel <daniel.costea@vkernel.ro>
2017-05-31 12:03:30 +0100

    Sign up feature test passing

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -8,10 +8,16 @@ RSpec.feature "Sessions",  type: :feature do
   	fill_in 'user_email', :with => email
   	fill_in 'user_password', :with => "password"
   	fill_in 'user_password_confirmation', :with => "password"
-    save_and_open_page
-
-  	click_button 'Sign up'
-    save_and_open_page
+    click_button 'Sign up'
     page.should have_content("Welcome! You have signed up successfully.")
+  end
+
+  scenario "signs in the user", :type => :feature do
+    visit "/users/sign_in"
+    email = "example@example.com"
+    fill_in 'user_email', :with => email
+    fill_in 'user_password', :with => "password"
+    click_button 'Log in'
+    page.should have_content("Signed in successfully.")
   end
 end
