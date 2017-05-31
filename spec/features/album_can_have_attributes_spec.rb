39968376f960f4b5fe4ee48daa33b0010862afd2
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-31 15:17:40 +0100

    failing test is failing for the correct reason

--- spec/features/album_can_have_attributes_spec.rb
+++ spec/features/album_can_have_attributes_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
-require '.app/models/photo_album'
+require './app/models/photo_album'
 
 feature 'photoalbums' do
-  scenario 'can have attributes name, location, date'
-  visit "/albums"
-  click_on("New album")
-  fill_in "Name", with "Test Album Name"
-  fill_in "Location", with "Test Location"
-  fill_in "Date", with "01/01/1970"
-  click_on("Create album")
-  expect(page).to have_content("Name: Test Album Name")
-  expect(page).to have_content("Location: Test Location")
-  expect(page).to have_content("Date: 01/01/1970")
+  scenario 'can have attributes name, location, date' do
+    visit "/albums"
+    click_on("New album")
+    fill_in "Name", with: "Test Album Name"
+    fill_in "Location", with: "Test Location"
+    fill_in "Date", with: "01/01/1970"
+    click_on("Create album")
+    expect(page).to have_content("Name: Test Album Name")
+    expect(page).to have_content("Location: Test Location")
+    expect(page).to have_content("Date: 01/01/1970")
+  end
 end
 
