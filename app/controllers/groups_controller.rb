45573eb35f6959a8cbb3819e34d302a83bea54e3
Sam <sblausten@Sams-Mac.local>
2017-05-31 11:55:27 +0100

    Feature test and implementation for rendering groups list but need group models to pass. Tried mocking it but gave up.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
 	def index
-		
+		@groups = groups.all
 	end
 
 end
--- spec/features/view_groups_spec.rb
+++ spec/features/view_groups_spec.rb
@@ -2,27 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "View groups", type: :feature do
 	before(:each) { 
-		group1 = double()
-		group2 = double()
-		groups = double() { [group1, group2] }
-		allow(group1).to receive(:title) {
-			"Group name 1"
-		}
-		allow(group2).to receive(:title) {
-			"Group name 2"
-		}
-		allow(group1).to receive(:description) {
-			"Group description 1"
-		}
-		allow(group2).to receive(:description) {
-			"Group description 2"
-		}
 	}
-
   scenario "Can visit a page to view list of all groups" do
     visit "/groups"
-    #expect(page.body).to have_css("li", count: 2)
-    expect(page.body).to have_content("Group name 1", "Group description 1")
-    #expect(page.body).to have_content("Group name 2", "Group description 2")
+    expect(page.body).to have_css("li", count: 2)
+    expect(page.body).to have_content("Group name 1", "Group description 2")
+    expect(page.body).to have_content("Group name 2", "Group description 2")
   end
 end
+
