48646eef782c2a388729780ff05e32eb48906348
Sophie Mann <sklm@live.co.uk>
2017-05-31 08:37:38 -0500

    adds method to show local time

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
   has_many :comments
   include PostPresenter
-  
+
   validates :message, presence: true
+
+  def local_time_stamp
+    self.created_at.localtime.strftime('%H:%M')
+  end
+
+  def local_date_stamp
+    self.created_at.localtime.strftime('%d/%m/%Y')
+  end
 end
- 
