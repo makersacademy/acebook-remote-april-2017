df40df7108859fb9fb2e5c6900aa4876fcd69456
danielemanuel <daniel.costea@vkernel.ro>
2017-05-31 12:24:28 +0100

    Refactoring the tests

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,40 +1,22 @@
 require 'rails_helper'
+require 'web_helpers'
 
 RSpec.feature "Sessions",  type: :feature do
 
   scenario "signs up the user", :type => :feature do
-    visit "/users/sign_up"
-  	email = "example@example.com"
-  	fill_in 'user_email', :with => email
-  	fill_in 'user_password', :with => "password"
-  	fill_in 'user_password_confirmation', :with => "password"
-    click_button 'Sign up'
+    sign_up
     page.should have_content("Welcome! You have signed up successfully.")
   end
 
   scenario "signs in the user", :type => :feature do
-    visit "/users/sign_up"
-    email = "example@example.com"
-  	fill_in 'user_email', :with => email
-  	fill_in 'user_password', :with => "password"
-  	fill_in 'user_password_confirmation', :with => "password"
-    click_button 'Sign up'
+    sign_up
     visit "/users/sign_out"
-    visit "/users/sign_in"
-    email = "example@example.com"
-    fill_in 'user_email', :with => email
-    fill_in 'user_password', :with => "password"
-    click_button 'Log in'
+    sign_in
     page.should have_content("Signed in successfully.")
   end
 
   scenario "signs in the user", :type => :feature do
-    visit "/users/sign_up"
-    email = "example@example.com"
-  	fill_in 'user_email', :with => email
-  	fill_in 'user_password', :with => "password"
-  	fill_in 'user_password_confirmation', :with => "password"
-    click_button 'Sign up'
+    sign_up
     visit "/users/sign_out"
     page.should have_content("Signed out successfully.")
   end
