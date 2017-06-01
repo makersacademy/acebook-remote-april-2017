494147658b3c8a3d418859136028c55438eee1df
Paula <pmuldoon@umich.edu>
2017-06-01 16:28:46 +0100

    photo comment test failing at undefined method photo_comments

--- app/controllers/photo_comments_controller.rb
+++ app/controllers/photo_comments_controller.rb
@@ -2,13 +2,13 @@ class PhotoCommentsController < ApplicationController
 
   def create
     @photo = Photo.find(params[:photo_id])
-    @comment = @photo.photo_comments.create(comment_params)
+    @comment = @photo.photo_comments.create(photo_comment_params)
     redirect_to photos_path
   end
 
   private
 
-  def comment_params
-    params.require(:comment).permit(:commenter, :body)
+  def photo_comment_params
+    params.require(:photo_comment).permit(:commenter, :body)
   end
 end
