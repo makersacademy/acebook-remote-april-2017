664ffc0c4dfe271a835eecd5ba1396fa4214e45a
david gray <davidseangray@gmail.com>
2017-06-01 16:48:23 +0100

    refactored for merge request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # include FormatUrl
+
   before_action :authenticate_user!
 
   def new
--- spec/features/signed_in_users_see_newsfeed_spec.rb
+++ spec/features/signed_in_users_see_newsfeed_spec.rb
@@ -8,7 +8,7 @@ feature 'Newsfeed visible only to signed in users' do
     expect(current_path).to eq('/')
   end
 
-  scenario 'user is not sign in and does not see the newsfeed' do
+  scenario 'user is not signed in and does not see the newsfeed' do
     visit ('/posts')
     expect(page).to have_content('You need to sign in or sign up before continuing.')
   end
