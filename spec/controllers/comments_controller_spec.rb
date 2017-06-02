8bd56705fc060a8053555228fdb051ca43d86de6
Robert Jones <robert.g.jones@gmail.com>
2017-06-02 14:47:03 +0100

    Fix breaking tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
   describe "POST create" do
     it "creates a comment" do
-      some_post = post(:create)
+      some_post = create(:post)
       post :create, params: { post_id: some_post.id, comment: { commenter: "Bob", body: "Comment", post_id: some_post.id } }
+
       expect(some_post.comments[0].body).to eq("Comment")
     end
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Comment, type: :model do
   let(:post){ create :post}
   let(:comment){ create :comment }
   it "has a body" do
-    expect(@comment.body).to eq("Comment")
+    expect(comment.body).to eq("Comment")
   end
   it "has a commenter" do
-    expect(@comment.commenter).to eq("example@example.com")
+    expect(comment.commenter).to eq("example@example.com")
   end
 end
