71c7edb86687d43164d2e7fb38caa29d1c17c6a2
Robert Jones <robert.g.jones@gmail.com>
2017-06-02 11:49:28 +0100

    Feature test for user details on their posts; unit test user association with posts; let posts belong_to users and user have many posts

---
+++ spec/features/posts_belong_to_a_user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+require 'web_helpers'
+
+RSpec.feature "Post belong to Users", type: :feature do
+
+  scenario "posts have their name on", :type => :feature do
+    sign_up
+    create_a_post("Expecting my credentials on this post")
+    expect(page).to have_content("example@example.com")
+    expect(page).to have_content("Expecting my credentials on this post")
+  end
+end
