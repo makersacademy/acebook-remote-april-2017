0ae84eb3188a4ead3a67de16d4ccb2309ebe5642
david gray <davidseangray@gmail.com>
2017-06-02 14:21:19 +0100

    users can only see photos if logged in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,20 @@
 FactoryGirl.define do
+
   factory :user do
-    trait :me do
-      email "example@example.com"
-      password "password"
-      password_confirmation"password"
-    end
-    trait :friend do
-      email "friend@example.com"
-      password "password"
-      password_confirmation"password"
-    end
+    email 'test1@test.com'
+    password 'password'
   end
+
+  trait :me do
+    email "example@example.com"
+    password "password"
+    password_confirmation"password"
+  end
+
+  trait :friend do
+    email "friend@example.com"
+    password "password"
+    password_confirmation"password"
+  end
+
 end
