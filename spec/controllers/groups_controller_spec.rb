154db2cc56afc20ecabc73c0adb7a157d6b648ec
Alex Chalk <alexchalk17@gmail.com>
2017-06-02 04:56:19 -0400

    Remove redundant tests

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -2,22 +2,10 @@ require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
 
-  describe "GET /new " do
-    xit "responds with 200" do
-      get :new
-      expect(response).to have_http_status(200)
-    end
-  end
-
-  describe "POST /" do
-    xit "responds with 200" do
-      group :create, params: { group: { name: "Group 1" } }
-      expect(response).to redirect_to(groups_url)
-    end
-
-    xit "creates a group" do
-      group = create(:group)
-      expect(Group.find_by(name: "Group 1")).to be
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
     end
   end
 
