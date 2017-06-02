d873ff42f26be627fa06685339ba4cceeb960275
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-06-02 12:00:47 +0100

    unpeded Photo Upload feature test, also fixed test. Changed button from Submit to Upload Photo

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -23,6 +23,14 @@ def create_a_post(text)
   click_button "Submit"
 end
 
+def upload_a_photo
+  visit "/photo"
+  click_on "New photo"
+  fill_in "Title", with: "test_photo_title"
+  page.attach_file('photo_image', Rails.root + 'app/assets/images/brighton.jpg')
+  click_button "Upload Photo"
+end
+
 def sign_up_friend
   visit "/users/sign_up"
   email = "friend@example.com"
