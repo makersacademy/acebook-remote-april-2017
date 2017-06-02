2928b3ff43f5912af173a983a30af2e5d48c9903
Robert Jones <robert.g.jones@gmail.com>
2017-06-02 15:23:10 +0100

    Posts belong to users and user's emails are rendered on a post

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-  has_many :posts
+  has_many :posts, :foreign_key => "user_id"
 end
