665f22b8f16548f16e00366fd6ba6285db341134
milesillsley <milesillsley@hotmail.co.uk>
2017-06-02 15:46:58 +0100

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601155007) do
+ActiveRecord::Schema.define(version: 20170601123750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 20170601155007) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_email"
   end
 
   create_table "users", force: :cascade do |t|
