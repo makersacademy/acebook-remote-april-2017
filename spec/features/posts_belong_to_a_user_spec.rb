4ee75b3e57742593739a4813014b9a39dbafefa3
Robert Jones <robert.g.jones@gmail.com>
2017-06-02 12:21:31 +0100

    Generate migration to add users to posts; break many tests in the process

--- spec/features/posts_belong_to_a_user_spec.rb
+++ spec/features/posts_belong_to_a_user_spec.rb
@@ -6,6 +6,7 @@ RSpec.feature "Post belong to Users", type: :feature do
   scenario "posts have their name on", :type => :feature do
     sign_up
     create_a_post("Expecting my credentials on this post")
+    visit "/posts"
     expect(page).to have_content("example@example.com")
     expect(page).to have_content("Expecting my credentials on this post")
   end
