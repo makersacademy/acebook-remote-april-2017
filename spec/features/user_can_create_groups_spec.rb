46be9b52c2a781e7f807cf3c2c6dd3ea29ab6b36
Alex Chalk <alexchalk17@gmail.com>
2017-06-02 07:42:47 -0400

    Update feature test to protect against slash bug

--- spec/features/user_can_create_groups_spec.rb
+++ spec/features/user_can_create_groups_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Groups",  type: :feature do
   feature "Create groups" do
     scenario "User can create a group" do
-      visit '/groups'
+      visit '/groups/'
       click_on 'new-group'
       fill_in 'group_title', with: 'Group 1'
       click_button 'Submit'
